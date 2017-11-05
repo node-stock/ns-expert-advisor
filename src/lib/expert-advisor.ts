1cddeae20131bf6c48d06a310cee137bc9c695f8
zlq4863947 <zlq4863947@gmail.com>
2017-11-06 05:21:40 +0900

    代码更新

--- src/lib/expert-advisor.ts
+++ src/lib/expert-advisor.ts
@@ -217,7 +217,7 @@ export class ExpertAdvisor {
         throw new Error(`持仓:${JSON.stringify(position)},卖出信号出错`);
       }
       Log.system.info(`获取持仓:${JSON.stringify(position)}`);
-      Log.system.info(`信号股价(${singal.price}) > 当前股价(${price}) && 盈利超过500(${price} - ${position.price} > 10)`);
+      Log.system.info(`信号股价(${singal.price}) > 当前股价(${price}) && 盈利超过1000(${price} - ${position.price} > 10)`);
       // 信号出现时股价 > 当前股价(股价下跌) && 并且盈利超过1000
       if (singal.price > price && price - position.price > 10) {
         Log.system.info('卖出信号出现后,股价下跌,立即卖出', price);
